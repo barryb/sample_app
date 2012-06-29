b033488c87587e26214789a2c71315e68826ad4d
Barry Byrne <barry.j.byrne@gmail.com>
2012-06-29 11:22:09 +0100

    Add mixed case email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -86,6 +86,16 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
 
   describe "return value of authenticate method" do
     before { @user.save }
